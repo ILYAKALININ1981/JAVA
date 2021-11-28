97aaf7a24fd580d30185370246c8adb70c5b1980
ILYAKALININ1981 <i89169153429@gmail.com>
2021-11-28 17:15:35 +0300

    16th Spring

---
+++ src/main/Java/Spring/StartController.java
@@ -0,0 +1,12 @@
+package Spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StartController {
+    @GetMapping ("/hello-world")
+    public String StartPage() {
+        return "hello";
+    }
+}
